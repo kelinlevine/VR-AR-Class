0f340b54f4180879a215e925d82336c75d7aa58d
Bluerie <kelin.levine@createthenext.com>
2019-08-05 16:31:46 -0400

    Monday

---
+++ Demo_1/Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    //Variables
+    public GameObject player2;
+    public float speed = 1.5f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.right * speed * Time.deltaTime;
+        player2.transform.position += Vector3.right * speed * Time.deltaTime;
+    }
+}
---
+++ VR Project/Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    //Variables
+    public float speed = 0.5f; //Go figure
+    public GameObject Camera; //Reference for the camera GameObject
+    //End of Variables
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            transform.position = transform.position + Camera.transform.forward * speed * Time.deltaTime;
+        } else if(Input.touchCount > 0)
+        {
+            transform.position = transform.position + Camera.transform.forward * speed * Time.deltaTime;
+        }
+    }
+}
