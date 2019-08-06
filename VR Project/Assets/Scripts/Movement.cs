ec4d42843c9387e3176fe3b49b376e598787483d
Bluerie <kelin.levine@createthenext.com>
2019-08-06 13:20:40 -0400

    basics almost done

--- VR Project/Assets/Scripts/Movement.cs
+++ VR Project/Assets/Scripts/Movement.cs
@@ -5,22 +5,59 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     //Variables
-    public float speed = 0.5f; //Go figure
-    public GameObject Camera; //Reference for the camera GameObject
+    public float speed = 0.5f; //The speed of our movement
+    public GameObject cameraa; //Reference for the camera
+    private Vector3 spawnPoint; //The respawn location
+    public GameObject bounds; //Reference for the boundaries that respawn the player
+    public GameObject Door; //Reference for the door
+    public GameObject Key; //Reference for the key
+    public bool gotKey = false; //Keeps track of if we got the key
+    public bool doorUnlocked = false; //Keeps track of if the door is unlocked
     //End of Variables
-    void Start()
+    void Start() // Runs when the game starts
     {
-        
+        spawnPoint = transform.position; //Sets the respawn point to where the player starts
     }
 
-    void Update()
+    private void OnTriggerEnter(Collider collide) //When it hits stuff
     {
+        if(collide.gameObject.tag == "Respawn")
+        {
+            transform.position = spawnPoint;
+        } else if(collide.gameObject.tag == "Door")
+        {
+            doorUnlocked = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Key")
+        {
+            gotKey = true;
+            Destroy(other.gameObject);
+        }
+    }
+    
+    void OpenDoor() //Function for when the door needs do be opened
+    {
+        Quaternion newRotation = Quaternion.AngleAxis(-90, Vector3.up);
+        Door.transform.rotation = Quaternion.Slerp(Door.transform.rotation, newRotation, 0.05f);
+        Door.tag = "Untagged";
+    }
+
+    void Update() //Runs every frame
+    {
+        if(doorUnlocked == true)
+        {
+            OpenDoor();
+        }
         if(Input.GetKey(KeyCode.Space))
         {
-            transform.position = transform.position + Camera.transform.forward * speed * Time.deltaTime;
+            transform.position = transform.position + cameraa.transform.forward * speed * Time.deltaTime;
         } else if(Input.touchCount > 0)
         {
-            transform.position = transform.position + Camera.transform.forward * speed * Time.deltaTime;
+            transform.position = transform.position + cameraa.transform.forward * speed * Time.deltaTime;
         }
     }
 }
