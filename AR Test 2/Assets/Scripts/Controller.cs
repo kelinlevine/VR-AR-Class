674820ba17ff053dc49d9a4770fb10008408df3a
Bluerie <kelin.levine@createthenext.com>
2019-08-07 16:33:09 -0400

    Touch function works with tunnel

---
+++ AR Test 2/Assets/Scripts/Controller.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    //Variables
+    public float speed = 1f;
+    public GameObject spawn_1;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+    }
+    void OnTriggerEnter(Collider gate)
+    {
+        if(gate.gameObject.tag == "Gate_1")
+        {
+            transform.position = spawn_1.transform.position;
+        }
+    }
+}
---
+++ VR Tunnel/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed;
+    public static float speed;
+    private int preTouchCount;
+    private bool tapping = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = moveSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Checking if it's tapping
+        if(tapping)
+        {
+            tapping = false;
+        } else if(preTouchCount != Input.touchCount)
+        {
+            if(preTouchCount == 0)
+            {
+                tapping = true;
+            }
+        }
+        preTouchCount = Input.touchCount;
+        //Update "starts" here
+        if(Input.GetKeyDown(KeyCode.Space) || tapping)
+        {
+            Debug.Log(Input.touchCount);
+        }
+    }
+}
